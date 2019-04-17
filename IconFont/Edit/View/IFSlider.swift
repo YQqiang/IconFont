d327e876a82c5dc2e1181de435c1a416c1e23cf6
yuqiang <1054572107@qq.com>
2019-04-17 23:52:22 +0800

    add slider file

---
+++ IconFont/Edit/View/IFSlider.swift
@@ -0,0 +1,15 @@
+//
+//  IFSlider.swift
+//  IconFont
+//
+//  Created by qiang yu on 2019/4/5.
+//  Copyright © 2019 yuqiang. All rights reserved.
+//
+
+import UIKit
+
+class IFSlider: IFBaseView {
+
+    
+
+}
